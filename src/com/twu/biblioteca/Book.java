659d27435ad5e5c15866e38bd12788a4f706132f
Pakawat Anekwiroj <pakawatanekwiroj@Pakawats-MacBook-Pro.local>
2019-10-11 00:21:18 +0800

    Add unit test for showing a book detail

--- src/com/twu/biblioteca/Book.java
+++ src/com/twu/biblioteca/Book.java
@@ -22,4 +22,8 @@ public class Book {
     public String getPublicationYear() {
         return publicationYear;
     }
+
+    public String getDetail() {
+        return "name: " + getName() + ", author: " + getAuthor() + ", year: " + getPublicationYear();
+    }
 }
--- test/com/twu/biblioteca/BibliotecaTest.java
+++ test/com/twu/biblioteca/BibliotecaTest.java
@@ -11,4 +11,10 @@ public class BibliotecaTest {
         String expectedWelcomeMessage = "Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!";
         assertEquals(expectedWelcomeMessage, BibliotecaApp.getWelcomeMessage());
     }
+
+    @Test
+    public void showBookDetail() {
+        Book book = new Book("nameA", "authorA", "2019");
+        assertEquals("name: nameA, author: authorA, year: 2019", book.getDetail());
+    }
 }
