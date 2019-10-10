97f05da3502943441c3606cd9365f8ef74d604aa
Pakawat Anekwiroj <pakawatanekwiroj@Pakawats-MacBook-Pro.local>
2019-10-11 00:29:28 +0800

    Show a list of all library books

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -5,6 +5,16 @@ public class BibliotecaApp {
     public static void main(String[] args) {
         String welcomeMessage = getWelcomeMessage();
         System.out.println(welcomeMessage);
+
+        Book[] books = new Book[2];
+        books[0] = new Book("nameA", "authorA", "2019");
+        books[1] = new Book("nameB", "authorB", "2019");
+
+        System.out.println();
+        System.out.println("a list of all library books:");
+        for(Book book: books) {
+            System.out.println(book.getDetail());
+        }
     }
 
     public static String getWelcomeMessage() {
