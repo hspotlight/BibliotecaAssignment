3edceffd0b18d281b844181128272ac28cc0f01d
Pakawat Anekwiroj <pakawatanekwiroj@Pakawats-MacBook-Pro.local>
2019-10-11 00:07:48 +0800

    Book - add name property

--- src/com/twu/biblioteca/Book.java
+++ src/com/twu/biblioteca/Book.java
@@ -1,12 +1,18 @@
 package com.twu.biblioteca;
 
 public class Book {
+    private String name;
     private String author;
     private String publicationYear;
 
-    public Book (String author, String publicationYear) {
+    public Book (String name, String author, String publicationYear) {
+        this.name = name;
         this.author = author;
         this.publicationYear = publicationYear;
+    }
+
+    public String getName() {
+        return name;
     }
 
     public String getAuthor() {
